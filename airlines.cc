1abc08d77ce27700dd093646aba3ddf46b661ef7
Jordi <vilaseca11@hotmail.com>
2014-12-30 17:51:24 +0100

    Added maxflow functions

--- airlines.cc
+++ airlines.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -20,6 +21,8 @@ struct Edge {
 };
 
 typedef vector<vector<Edge>> Graph;
+typedef vector< vector <pair<int,int> > > IImat;		// Següent node, capacitat aresta.
+typedef vector< pair<int,int> > IIvec;					// Següent node, capacitat aresta.
 
 // *** Global variables ***
 
@@ -104,6 +107,123 @@ void updateK(Graph& g, int k) {
 	g[s][0].cap = k;
 }
 
+// Funció que crea el graf residual inicial associat a un graf.
+IImat graf_to_grafRes(const Graph & graf) {
+	IImat aux = IImat (graf.size());
+	for (int i = 0; i < (int) graf.size(); ++i) {
+		IIvec auxII (graf[i].size());
+		for (int j = 0; j < (int) graf[i].size(); ++j) {
+			auxII[j] = make_pair(graf[i][j].to, graf[i][j].cap);
+		}
+		aux[i] = auxII;
+	}
+	return aux;
+}
+
+// Funció que a partir de un graf residual calcula el camí entre l'inici i la fi del graf (si ni ha).
+// La funció retorna el valor del bottleneck del camí i modifica el vector "cami" amb aquest si el camí existeix.
+// En cas contrari la funció retorna com a bottleneck 0 i el valor del vector "cami" conté dades erronies.
+int trobar_cami(const IImat & grafRes, vector<int> & cami) {
+	
+	// Inicialitzem el bottleneck a 0, si no es troba cap camí aquest serà el valor retornat.
+	
+	int bottleneck = 0;
+	// Eliminem l'informació que contenia cami i l'inicialitzem a -1.
+	cami = vector<int> (grafRes.size(), -1);
+	// per a qualsevol vèrtex i, 
+	// [i] = capacitat màxima que pot passar des de l'inici fins a i.
+	vector<int> capacitat = vector<int> (grafRes.size(), 0);
+
+	queue<int> vertexs;
+	vertexs.push(S);
+	// La capacitat des del inici fins a ell mateix és infinit.
+	
+	capacitat[S] = 10000;						// ------------S'ha de corregir, solució parcial. ------------
+	// Evitem que el camí pugui passar dos cops per el vèrtex inicial.
+	cami[S] = -2;
+
+	while (not vertexs.empty()) {
+		int vertexIni = vertexs.front();
+		vertexs.pop();
+
+		for(int i = 0; i < (int) grafRes[vertexIni].size(); i++) {
+
+			int vertexAct = grafRes[vertexIni][i].first;
+			int capacitatTot = grafRes[vertexIni][i].second;
+			int capacitatRes = capacitatTot - capacitat[vertexAct];
+
+			if (capacitatRes > 0 and cami[vertexAct] == -1) {
+				cami[vertexAct] = vertexIni;
+				capacitat[vertexAct] = min(capacitat[vertexIni], capacitatRes);
+				if (vertexAct == T) return capacitat[T];
+				vertexs.push(vertexAct);
+			}
+		}
+	}
+	return bottleneck;
+
+}
+
+// Funció que busca dins el graf residual a quina posició es troba el vèrtex desitjat. En cas de no existir
+// el crea i retorna l'index del vector on es troba.
+int troba_posicio(IImat & grafRes, int posIni, int posFi) {
+	for (int i = 0; i < (int) grafRes[posIni].size(); ++i) {
+		if (grafRes[posIni][i].first == posFi) return i;
+	}
+
+	// No s'ha trobat node, el crea amb flux inicial 0.
+	grafRes[posIni].push_back(make_pair(posFi, 0));
+	return grafRes[posIni].size()-1;
+}
+
+// Funció que a partir del graf residual, un camí i el bottleneck d'aquest camí actualitza els valors
+// dels nodes que formen part del camí del graf residual.
+void augment(IImat & grafRes, const vector<int> & cami, int bottleneck) {
+	int vertexAct = T;
+	while (vertexAct != S){
+		int vertexIni = cami[vertexAct];
+
+		// Restem el valor del bottleneck al flux que va en el sentit del camí
+		int posMat2 = troba_posicio(grafRes, vertexIni, vertexAct);
+		grafRes[vertexIni][posMat2].second -= bottleneck;
+
+		// Sumem el valor del bottleneck al flux que va en sentit contrari al camí
+		int posMat1 = troba_posicio(grafRes, vertexAct, vertexIni);
+		grafRes[vertexAct][posMat1].second += bottleneck;
+
+		vertexAct = cami[vertexAct];
+	}
+}
+
+// Funció que a partir del graf, un camí i el bottleneck d'aquest camí actualitza els valors dels
+// nodes que formen part del camí del graf.
+void actualitza_graf(Graph & graf, const vector<int> & cami, int bottleneck) {
+	int vertexAct = T;
+	while (vertexAct != S){
+		int vertexIni = cami[vertexAct];
+
+		int i = -1;
+		while(graf[vertexIni][++i].to != vertexAct);		//Busca la posició del vèrtex en la llista d'adjacències.
+		graf[vertexIni][i].flow += bottleneck;
+
+		vertexAct = cami[vertexAct];
+	}
+}
+
+// Funció que executa el max flow d'un graf.
+int max_flow(Graph & graf) {
+	IImat grafRes = graf_to_grafRes(graf);
+	vector<int> cami;
+	int bottleneck;
+	int maxflow = 0;
+	while ((bottleneck = trobar_cami(grafRes, cami)) > 0) {
+		augment(grafRes, cami, bottleneck);
+		actualitza_graf(graf, cami, bottleneck);
+		maxflow += bottleneck;
+	}
+	return maxflow;
+}
+
 int main(int argc, char *argv[]) {
 	// X defines the capacity of the edges in the different versions
 	int X = -1;
@@ -118,12 +238,15 @@ int main(int argc, char *argv[]) {
 	readFlights(flights,airports,X);
 
 	// number of pilots to serve all flights
-	int k = 0;
+	int k = 2;
 	// Graph that represents the network flow
 	Graph g(flights.size()*2+4);
 	buildGraph(g,flights,airports,k,X);
 	printGraph(g);
 
-	// solve maxflow
+	Graph g1 = g;
+	int maxflow = max_flow(g1);
+	cout << endl << "Final graph with value " << maxflow << ":" << endl;
+	printGraph(g1);
 	return 0;
 }
